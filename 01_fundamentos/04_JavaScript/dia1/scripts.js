9f57471a37b8adb129055c72725a60d78ff6b8d4
Vitor <vbrenochacon@hotmail.com>
2022-07-20 16:04:25 -0300

    Exercicios Switch case concluídos

---
+++ 01_fundamentos/04_JavaScript/dia1/scripts.js
@@ -0,0 +1,48 @@
+// const myName = 'Vitor'
+// const birthCity = 'João Pessoa'
+// let birthYear = 1997
+
+// birthYear = 2030
+// birthCity = 'Xablau'
+
+// console.log(myName)
+// console.log(birthCity)
+// console.log(birthYear)
+
+// const base = 5
+// const altura = 8
+// const perimetro = base + altura
+
+// console.log(typeof base)
+// console.log(typeof altura)
+// console.log(typeof perimetro)
+
+// const notaTecnica = 40
+
+// if (notaTecnica >= 80) {
+//     console.log('Parabéns você foi aprovado!')
+// }else if (notaTecnica < 80 && notaTecnica >= 60) {
+//     console.log('Você está na nossa lista de espera.')
+// }else {
+//     console.log('Você foi reprovado(a)')
+// };
+
+let tv = 'canal terra plana'
+
+switch (tv) {
+    case 1:
+        console.log('A tv está no canal 1')
+        break
+
+    case 2:
+        console.log('A tv está no canal 2')
+        break
+    case 3:
+        console.log('A tv está no canal 3')
+        break
+    case 4:
+        console.log('A tv está no canal 4')
+        break
+    default:
+        console.log('A tv não está em nenhum dos canais reconhecidos')
+}
