97b3c5408ddc61233b7f60b9dc6a1ffbfff04e8b
Vitor <vbrenochacon@hotmail.com>
2022-07-21 15:48:04 -0300

    arrays

---
+++ 01_fundamentos/04_JavaScript/dia2/scripts.js
@@ -0,0 +1,8 @@
+// Arrays
+
+let menu = ['home', 'serviços', 'portfólio', 'links'];
+let menusevices = menu[1];
+let indexOfPortifolio = menu.indexOf('portfólio')
+menu.push('contato')
+
+console.log(indexOfPortifolio)
